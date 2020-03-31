ae4be17f3113fe250db2c80b9b837a77ea2e0ea8
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2020-03-31 12:35:34 -0700

    added static path for nim

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nim compile --outdir=./../../../../home/pi/Documents/ Main.nim 
+/home/pi/nim-1.1.1/bin/nim compile --outdir=./../../../../home/pi/Documents/ Main.nim 
 mv /home/pi/Documents/Main Main
